f41f08556eeaf834a27f1cb8fdd39e9f319e7c69
Budhi Darma Purwanta <BudhiDarmaP@users.noreply.github.com>
2017-06-21 20:51:58 +0800

    Add files via upload

---
+++ CSSD/application/models/Instrument.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Description of User
+ *
+ * @author budhidarmap
+ */
+class Instrument extends CI_Model {
+   
+    function panggil_semua_data_instrument() {
+        $q = $this->db->query("SELECT * FROM `instrument`");
+        return $q->result;
+    }
+    function panggil_data_instrument() {
+        $q = $this->db->query("SELECT * FROM `instrument` WHERE STERIL > 0");
+        return $q->result;
+    }
+    function panggil_data_jumlah() {
+        $q = $this->db->query("SELECT * FROM `instrument` WHERE STERIL > 0");
+        return $q->result;
+    }
+}
